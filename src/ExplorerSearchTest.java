7e6a578c19802b33770ea898d06ea71e2dc68e31
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-05-31 17:36:13 -0700

    Added more tests for Explorer Search

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -25,6 +25,87 @@ public class ExplorerSearchTest {
         assertEquals(14, actual);
     }
 
+    @Test
+    public void testReachableArea_topCornerStart() {
+        int[][] island = {
+            {0,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,1,3,3},
+            {3,1,2,1,2,1},
+            {1,1,1,2,1,1},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(3, actual);
+    }
+
+    @Test
+    public void testReachableArea_topRightStart() {
+        int[][] island = {
+            {1,1,1,3,1,0},
+            {3,2,3,1,3,1},
+            {1,1,1,1,3,3},
+            {3,1,2,1,2,1},
+            {1,1,1,2,1,1},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(3, actual);
+    }
+
+    @Test
+    public void testReachableArea_bottomLeftStart() {
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,1,3,3},
+            {3,1,2,1,2,1},
+            {0,1,1,2,1,1},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(10, actual);
+    }
+
+    @Test
+    public void testReachableArea_bottomRightStart() {
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,1,3,3},
+            {3,1,2,1,1,1},
+            {2,1,1,2,1,0},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(13, actual);
+    }
+
+    @Test
+    public void testReachableArea_allUnreachableExceptStart() {
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,1,3,3},
+            {3,1,2,1,1,2},
+            {2,1,1,2,3,0},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(1, actual);
+    }
+
+    @Test
+    public void testReachableArea_NoExplorerPresent() {
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,1,3,3},
+            {3,1,2,1,1,2},
+            {2,1,1,2,3,1},
+        };
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            ExplorerSearch.explorerLocation(island);
+        });
+
+        assertEquals("No explorer on the island.", exception.getMessage());
+    }
+
     // Add more tests here!
     // Come up with varied cases
     @Test public void testExplorerLocation_topLeftCorner() {
